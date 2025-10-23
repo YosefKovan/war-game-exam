5178c73ab23d53b8cfd76ebe9ba149daefea2e00
Yossi Kovan <yossikovan@gmail.com>
2025-10-23 12:52:13 +0300

    adding the rest of the project

--- game_logic/game.py
+++ game_logic/game.py
@@ -1,4 +1,4 @@
-from utils.deck import create_deck, shuffle
+from utils.deck import create_deck, shuffle, compare_cards
 
 #=============================
 #        create player
@@ -40,6 +40,7 @@ def split_deck(deck):
 #=============================
 #         init game
 #=============================
+
 def init_game() -> dict:
 
     player1 = create_player()
@@ -56,15 +57,119 @@ def init_game() -> dict:
     player1["hand"],  player2["hand"] = split_deck(deck)
 
     #this returns the game dict
-    return {
-        "deck" : deck,
-        "player_1" : player1,
-        "player2" : player2
-    }
+    game_dict = { "deck" : deck, "player_1" : player1, "player_2" : player2}
+
+    return game_dict
 
 
+#=============================
+#         play round
+#=============================
 def play_round(p1 : dict, p2 : dict):
-    pass
+
+    p1_cards = p1['hand']
+    p2_cards = p2['hand']
+
+    #this will make sure the the players have cards left
+    if len(p1_cards) == 0 or len(p2_cards) == 0:
+       print("no cards left in pile exiting game!")
+       return
+
+    p1_card = p1_cards.pop(0)
+    p2_card = p2_cards.pop(0)
+
+    compare_result = compare_cards(p1_card, p2_card)
+
+    match compare_result:
+        case "p1" :
+            #this will add to the won pile array - both cards
+            #print("player 1 Won this round")
+            p1["won_pile"].extend([p1_card, p2_card])
+        case "p2":
+            p2["won_pile"].extend([p1_card, p2_card])
+            #print("player 2 Won this round")
+        case "WAR":
+            print("WAR TIED OCCURRED")
+
+#=============================
+#     draw cards (bonus)
+#=============================
+
+#this function is not finished
+def draw_cards(cards, amount_of_cards):
+    """this function will draw a specified amount of cards from the players cards"""
+    drawn_cards = []
+    for i in range(0, amount_of_cards):
+        drawn_cards.append(cards.pop(0))
+
+    return drawn_cards
+
+#=============================
+#     handle tie (bonus)
+#=============================
+
+#this function is not finished
+def handle_tie_in_game(p1, p2):
+
+   tie_over = False
+
+   while not tie_over:
+
+        #this can happen only if both players have 3 cards
+        if len(p1["hand"]) > 4 and len(p2["hand"]) > 4:
+
+            upside_down_cards_p1 = draw_cards(p1["hand"], 3)
+            upside_down_cards_p2 = draw_cards(p2["hand"], 3)
+
+            last_cards_p1 = p1["hand"].pop(0)
+            last_cards_p2 = p2["hand"].pop(0)
+
+            compare_result = compare_cards(last_cards_p1, last_cards_p2)
+
+            winner_pile = upside_down_cards_p1 + upside_down_cards_p2 + [last_cards_p1, last_cards_p2]
+
+            if compare_result == "p1":
+                p1["won_pile"].extend(winner_pile)
+                tie_over = True
+            elif compare_result == "p2":
+                p2["won_pile"].extend(winner_pile)
+                tie_over = True
+
+#=============================
+#   count suite card bonus
+#=============================
+def count_suite_card(cards, winning_suite):
+    """this function will count the amount of a specific suite"""
+
+    counter = 0
+
+    for card in cards:
+        if card["suite"] == winning_suite:
+            counter += 1
+
+    return counter
+
+#=============================
+#   handle tie result (bonus)
+#=============================
+def handle_game_result_tie(p1,p2):
+    """if there was a tie this function will check according to the suite of the card"""
+
+    current_winning_suite = "H"
+
+    p1_winning_suite_count = count_suite_card(p1["won_pile"], current_winning_suite)
+    p2_winning_suite_count = count_suite_card(p2["won_pile"], current_winning_suite)
+
+    if p1_winning_suite_count > p2_winning_suite_count:
+        print(f"the winning player is : {p1["name"]}")
+    elif p2_winning_suite_count < p2_winning_suite_count:
+        print(f"the winning player is : {p2["name"]}")
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from game_logic.game import init_game
-
+from game_logic.game import init_game, play_round
 
 
 if __name__ == "__main__":
-   init_game()
+   game_data = init_game()
+   p1, p2 = game_data["player_1"], game_data["player_2"]
+   play_round(p1, p2)
+
+
+
